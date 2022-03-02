f1715605408aafd1d608fd420b84f622224b6b2e
Jordan Cook <jcook3195@gmail.com>
2022-03-01 22:36:15 -0500

    added very basic category display loop

--- single-hikes.php
+++ single-hikes.php
@@ -5,6 +5,12 @@
 
 get_header();
 
+// get the categories for the post
+$categories = get_categories( array(
+    'orderby' => 'name',
+    'order'   => 'ASC'
+) );
+
 // convert trail length in miles to km with 1 number after decimal
 $trail_length_km = round((get_field('trail_length') * 1.609344), 1);
 
@@ -102,6 +108,18 @@ if( $trail_type == 'out_and_back' ) {
             <div class="single-adventure-description">
                 <?php the_field('trail_description'); ?>
             </div>
+        </div>
+        <div class="single-adventure-categories-container">
+            <?php                                
+                foreach( $categories as $category ) { ?>
+                <div class="single-category">
+                    <?php
+                        echo '<p>' . $category->name . '</p>';
+                    ?>
+                </div>
+                <?php
+                } 
+            ?>
         </div>
     </div>
 </div>
